e1e44b995b085e6584d568016345ebd32ad85f2d
SvetlanaPoluhina <47693962+SvetlanaPoluhina@users.noreply.github.com>
2020-05-09 19:18:48 +0500

    EFCore

---
+++ EFCore/EFCore/Model.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCore
+{
+    public class ApplicatContext : DbContext
+    {
+        public DbSet<Tester> Testers { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(ConfigurationManager.ConnectionStrings["SystemManagerTestsDatabase"].ConnectionString);
+        }
+    }
+
+    [Table("tester")]
+    public class Tester
+    {
+        [Column("id_tester")]
+        public int Id { get; set; }
+        [Column("name_tester")]
+        public string Name { get; set; }
+    }
+
+}
---
+++ EFCore/EFCore/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace EFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new ApplicatContext())
+            {
+                // Create
+                Console.WriteLine("Добавление нового тестера");
+                db.Add(new Tester { Name = "Ivanov Ivan" }); //Введите имя, которое хотите добавить
+                db.SaveChanges();
+
+                // Update
+                Console.WriteLine("Обновление");
+                int id = 3; //Введите id тестера, чье имя хотите обновить
+                Tester new_tester = db.Testers.Find(id); 
+                //Tester new_tester = db.Testers.FirstOrDefault(); //Обновление по первому id
+                if (new_tester != null)
+                {
+                    new_tester.Name = "Socolov Kirill"; //Введите новое имя тестера
+                    db.Testers.Update(new_tester);
+                    db.SaveChanges();
+                }
+
+                // Delete
+                Console.WriteLine("Удаление");
+                int id_delete = 6; //Введите id тестера, которого хотите удалить
+                Tester tester = db.Testers.Find(id_delete);
+                //Tester tester = db.Testers.FirstOrDefault(); //Удаление по перому id
+                if (new_tester != null)
+                {
+                    db.Remove(tester);
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
